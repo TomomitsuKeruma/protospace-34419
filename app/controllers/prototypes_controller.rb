8edff8f19e8b6dab905bf81fceaac1eb9ab3058f
TomomitsuKeruma <78133385+TomomitsuKeruma@users.noreply.github.com>
2021-02-16 12:43:45 +0900

    initial commit_2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_edit, only: [:edit]
 
   def index
     @prototypes = Prototype.includes(:user) 
@@ -26,9 +27,6 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to root_path
-    end
   end
 
   def update
@@ -49,6 +47,13 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_edit
+    prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == prototype.user_id
+      redirect_to root_path
+    end
   end
 
 end
